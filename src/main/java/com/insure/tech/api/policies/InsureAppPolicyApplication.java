f9d9145fb649df007443669f89cf56a1d2d8b093
bhupender <bhupender@bhupender-LP>
2020-03-27 13:49:49 +0530

    Register with Eureka server

--- src/main/java/com/insure/tech/api/policies/InsureAppPolicyApplication.java
+++ src/main/java/com/insure/tech/api/policies/InsureAppPolicyApplication.java
@@ -2,7 +2,9 @@ package com.insure.tech.api.policies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InsureAppPolicyApplication {
 
